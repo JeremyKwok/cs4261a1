1e25edc9ba42602dab7d0cfa12b9781ebdbf5da7
JeremyKwok <jeremykke@gmail.com>
2020-04-06 17:59:30 +0800

    linked model to firestore service

--- lib/services/firestore.dart
+++ lib/services/firestore.dart
@@ -1,6 +1,8 @@
 import 'dart:async';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import '../models/user.dart';
+import '../models/group.dart';
+import '../models/event.dart';
 
 final Firestore db = Firestore.instance;
 
