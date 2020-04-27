8ae90d5c5cc5fc8c8c709a7ca3f7fb82e7a11a92
= <jleuthold3@gatech.edu>
2020-04-27 14:48:39 -0400

    UI changes to the tab bar

--- lib/ui/pages/home_page.dart
+++ lib/ui/pages/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:cs4261a1/ui/widgets/input_field.dart';
 import 'package:flutter/material.dart';
 import 'package:cs4261a1/services/authentication.dart';
 
@@ -140,7 +141,7 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return new DefaultTabController(length: 3, child: Scaffold(
+    return new DefaultTabController(length: 2, child: Scaffold(
         appBar: new AppBar(
           title: new Text(widget.params['appName']),
           actions: <Widget>[
@@ -153,23 +154,35 @@ class _HomePageState extends State<HomePage> {
         body: 
           new TabBarView(
             children: <Widget>[
-              new Image.asset('assets/coronavirus_us.png'),
 
-              new Text(
-              " Ongoing",
-                style: new TextStyle(fontSize:30.0,
-                color: const Color(0xFF000000),
-                fontWeight: FontWeight.w200,
-                fontFamily: "Roboto"),
+              new Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    "COVID-19 News",
+                    style: new TextStyle(
+                        fontSize: 30.0,
+                        color: const Color(0xFF000000),
+                        fontWeight: FontWeight.w200,
+                        fontFamily: "Roboto"
+                    ),
+                  ),
+                  Image.asset('assets/coronavirus_us.png'),
+                ],
               ),
     
-              new Text(
-              " Symptoms",
-                style: new TextStyle(fontSize:30.0,
-                color: const Color(0xFF000000),
-                fontWeight: FontWeight.w200,
-                fontFamily: "Roboto"),
-              ),
+              new Column (
+               children: <Widget> [
+                 Text(
+                   "My Symptoms",
+                   style: new TextStyle(fontSize:30.0,
+                       color: const Color(0xFF000000),
+                       fontWeight: FontWeight.w200,
+                       fontFamily: "Roboto"),
+                 ),
+               ]
+              )
+
 
             ]
     
@@ -180,7 +193,7 @@ class _HomePageState extends State<HomePage> {
           items: [
             new BottomNavigationBarItem(
               icon: const Icon(Icons.access_time),
-              title: new Text('My Symptoms'),
+              title: new Text('My Symptoms Notes'),
             ),
     
             new BottomNavigationBarItem(
@@ -214,15 +227,20 @@ class SubPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('My Symptoms'),
+        title: Text('My Symptom Notes'),
         backgroundColor: Colors.green,
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
           children: <Widget>[
-            Text('Your reported symptoms from the past 14 days'),
-
+            Text('Record your experiences and symptoms here'),
+            TextField(
+              decoration: InputDecoration(
+                border: InputBorder.none,
+                hintText: 'How are you feeling?'
+              ),
+            )
 
           ],
         ),
@@ -247,7 +265,6 @@ class SymptomPage extends StatefulWidget {
   }
 }
 
-String dropdownValue = "select";
 class _QueuePage extends State<QueuePage> {
   String dropdownValue = 'select';
 
@@ -290,7 +307,7 @@ class _QueuePage extends State<QueuePage> {
 
 class _SymptomPage extends State<SymptomPage> {
 
-  @override
+
 
   bool hasCough = false;
   bool hasFever = false;
@@ -301,15 +318,15 @@ class _SymptomPage extends State<SymptomPage> {
   bool hasSoreThroat = false;
   bool hasLossofSense = false;
 
-  var _curIndex = 0;
   var contents = "Home";
 
-
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Scaffold(
           appBar: AppBar(
             title: Text('Check all Symptoms you have'),
+            backgroundColor: Colors.green,
           ),
             body: Container(
               child: Column(
