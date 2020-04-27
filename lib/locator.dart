42a6011e7f9a6a61a33d477a9901f8875ad6afb5
JeremyKwok <jeremykke@gmail.com>
2020-04-28 01:11:39 +0800

    add dialog manager and service to locator

--- lib/locator.dart
+++ lib/locator.dart
@@ -2,13 +2,13 @@ import 'services/authentication.dart';
 import 'services/firestore.dart';
 import 'package:get_it/get_it.dart';
 import 'services/navigation.dart';
-//import 'services/dialog_service.dart';
+import 'services/dialog.dart';
 
 final GetIt locator = GetIt();
 
 void setupLocator() {
   locator.registerLazySingleton(() => NavigationService());
-  //locator.registerLazySingleton(() => DialogService());
+  locator.registerLazySingleton(() => DialogService());
   locator.registerLazySingleton(() => Auth());
   locator.registerLazySingleton(() => FirestoreService());
 }
