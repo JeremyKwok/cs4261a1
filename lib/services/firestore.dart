0c47ef000a3aefa9e564acd2c28b52b458468c23
TalhaAhsan <42753976+TalhaAhsan@users.noreply.github.com>
2020-04-27 06:23:02 -0400

    Add files via upload

---
+++ lib/services/firestore.dart
@@ -0,0 +1,100 @@
+import 'dart:async';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import '../models/status.dart';
+import '../models/status.dart';
+import '../models/user.dart';
+import '../models/group.dart';
+import '../models/event.dart';
+
+final Firestore db = Firestore.instance;
+
+class FirestoreService {
+  static final FirestoreService _instance = new FirestoreService.internal();
+  factory FirestoreService() => _instance;
+  FirestoreService.internal();
+  Future<Event> createEvent(String activity, String timing, String uid, int pax) async {
+    DocumentReference dr = db.collection(timing).document(activity);
+    final TransactionHandler createTransaction = (Transaction tx) async {
+      final DocumentSnapshot ds = await tx.get(dr.collection(uid).document('fields'));
+      final Event event = new Event(id: ds.documentID, userId: uid, pax: 6, attending: 0);
+      final Map<String, dynamic> data = event.toJson();
+      await tx.set(ds.reference, data);
+      return data;
+    };
+    await createGroup(timing, activity, uid, uid, pax);
+    return Firestore.instance.runTransaction(createTransaction).then((mapData){
+      return Event.fromJson(mapData);
+    }).catchError((error) {
+      print('error: $error');
+      return null;
+    });
+  }
+  Future<Group> createGroup(String timing, String activity, String uid, String id, int pax) async {
+    DocumentReference dr = db.collection(timing).document(activity);
+    final TransactionHandler createTransaction = (Transaction tx) async {
+      final DocumentSnapshot ds = await tx.get(dr.collection(uid).document());
+      final Group group = new Group(id: ds.documentID, userId: id, pax: pax);
+      final Map<String, dynamic> data = group.toJson();
+      await tx.set(ds.reference, data);
+      return data;
+    };
+    return db.runTransaction(createTransaction).then((mapData){
+      return Group.fromJson(mapData);
+    }).catchError((error) {
+      print('error: $error');
+      return null;
+    });
+  }
+  Future<dynamic> updateEvent(String timing, String activity, String uid, Event event, int pax) async {
+    DocumentReference dr = db.collection(timing).document(activity).collection(uid).document('fields');
+    final TransactionHandler updateTransaction = (Transaction tx) async {
+      final DocumentSnapshot ds = await tx.get(dr);
+
+      await tx.update(ds.reference, event.toJson());
+      return {'updated': true};
+    };
+
+    return db
+        .runTransaction(updateTransaction)
+        .then((result) => result['updated'])
+        .catchError((error) {
+      print('error: $error');
+      return false;
+    });
+  }
+  Future<Status> createStatus(  String id, String uid, int temp, bool tiredness, bool bodyache, bool nasalCongestion, bool soreThroat, bool diarrhoea) async {
+    DocumentReference dr = db.collection("symptoms").document(uid);
+    final TransactionHandler createTransaction = (Transaction tx) async {
+      final DocumentSnapshot ds = await tx.get(dr.collection(uid).document('fields'));
+      final Status status = new Status(id: ds.documentID, userId: uid, temp: temp, tiredness: tiredness, bodyache: bodyache, nasalCongestion: nasalCongestion, soreThroat: soreThroat, diarrhoea: diarrhoea);
+      final Map<String, dynamic> data = status.toJson();
+      await tx.set(ds.reference, data);
+      return data;
+    };
+    return Firestore.instance.runTransaction(createTransaction).then((mapData){
+      return Status.fromJson(mapData);
+    }).catchError((error) {
+      print('error: $error');
+      return null;
+    });
+  }
+  Future<dynamic> updateStatus(  String id, String uid, int temp, bool tiredness, bool bodyache, bool nasalCongestion, bool soreThroat, bool diarrhoea, Status status) async {
+    DocumentReference dr = db.collection("symptoms").document(uid);
+    final TransactionHandler updateTransaction = (Transaction tx) async {
+      final DocumentSnapshot ds = await tx.get(dr);
+
+      await tx.update(ds.reference, status.toJson());
+      return {'updated': true};
+    };
+
+    return db
+        .runTransaction(updateTransaction)
+        .then((result) => result['updated'])
+        .catchError((error) {
+      print('error: $error');
+      return false;
+    });
+  }
+
+
+}
